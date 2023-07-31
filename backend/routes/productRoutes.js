4996438ad2b2e1fe70b15c65577dc4abb4d3f29c
akshaya <akshayask29@gmail.com>
2023-07-31 12:32:39 +0530

    Add custom error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,8 +13,10 @@ router.get('/:id', asyncHandler(async(req, res)=>{
     const product = await Product.findById(req.params.id)
     if(product){
         res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('product not found')
     }
-    res.status(404).json('product not found')
 }))
 
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv'
 dotenv.config()
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000
 
@@ -15,6 +16,9 @@ app.get('/', (req, res)=> {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
